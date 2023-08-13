0d62616a95727fba1228056c41a1c255eb8816a3
Andrey Golyachenko <leswar@yandex.ru>
2023-08-13 18:52:32 +0500

    Complited project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+//#define SPRING
+#define SUMMER
+//#define FALL
+//#define WINTER
+
 int main()
 {
+
+	#ifdef SPRING
+		std::cout << "Spring";
+	#elif defined SUMMER
+		std::cout << "Summer";
+	#elif defined FALL
+		std::cout << "Fall";
+	#elif defined WINTER
+		std::cout << "Winter";
+	#else
+		std::cout << "No season";
+	#endif
+
 	return 0;
 }
